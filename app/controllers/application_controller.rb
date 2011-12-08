1a39adf13efcc59e156b2ccb15fd3ffe6c8f3291
Americo Savinon <americos@gmail.com>
2011-12-08 16:16:33 -0500

    TESTING using the ||= at the helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   
     def current_user
       #@current_user ||= User.find(session[:user_id]) if session[:user_id]
-      @current_user = session[:user] if session[:user]
+      @current_user ||= session[:user] if session[:user]
     end
   
 end
